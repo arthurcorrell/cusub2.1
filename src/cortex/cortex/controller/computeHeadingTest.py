fd1a4563e6754d7afa34ca7d52a1f50296f15b04
Arthur Correll <arthurcorrell@gmail.com>
2025-04-28 20:24:43 -0600

    Object dims and camera metadata from yaml file

--- src/cortex/cortex/controller/computeHeadingTest.py
+++ src/cortex/cortex/controller/computeHeadingTest.py
@@ -1,5 +1,6 @@
 import rclpy
 import rclpy.node as rcl
+import yaml
 
 # behaviortree package
 import behavior_tree.BehaviorTree as BehaviorTree
@@ -28,13 +29,18 @@ class BTRoot(rcl.Node):
         
         self.blackboard['current_object'] = cur_obj
 
-        # initialize blackboard (TODO: from JSON)
-        self.blackboard[cur_obj] = [0.215, 0.089]
+        # get current object dimensions
+        with open('src/cfg/object_properties.yaml') as f:
+            file = yaml.safe_load(f)
+            self.blackboard[cur_obj] = file['cur_object']
 
-        self.blackboard['focal_length'] = 0.0025
-        self.blackboard['image_size'] = [640, 480]
-        self.blackboard['sensor_size'] = [3.673e-3, 2.738e-3]
-        self.blackboard['fov'] = [95, 70]
+        # get camera properties
+        with open('src/cfg/camera_properties.yaml') as f:
+            file = yaml.safe_load(f)
+            self.blackboard['focal_length'] = file['focal_length']
+            self.blackboard['image_size'] = file['image_size']
+            self.blackboard['sensor_size'] = file['sensor_size']
+            self.blackboard['fov'] = file['fov']
 
         # subtree nodes
 
