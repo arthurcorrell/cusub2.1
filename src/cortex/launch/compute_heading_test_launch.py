b37bbfe46c214290614a95f708f5bba1bf90b45d
Arthur Correll <arthurcorrell@gmail.com>
2025-05-01 16:02:16 -0600

    foxglove bridge in launch file

--- src/cortex/launch/compute_heading_test_launch.py
+++ src/cortex/launch/compute_heading_test_launch.py
@@ -23,5 +23,14 @@ def generate_launch_description():
             executable='camera_node_raw',
             name='CameraPublisherRaw',
             parameters=[{'camera_port':2, 'fps':15}]
+        ),
+        Node(
+            package='foxglove_bridge',
+            executable='foxglove_bridge',
+            name='foxglove_bridge',
+            output='screen',
+            parameters=[
+                {'port': 8765}  # optional: default is 8765
+            ]
         )
     ])
