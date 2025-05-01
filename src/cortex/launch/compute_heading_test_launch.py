1ad4c78ae10261d0e57df8ffd3c2bb2d34c77b07
Arthur Correll <arthurcorrell@gmail.com>
2025-05-01 12:46:32 -0600

    added launch file

---
+++ src/cortex/launch/compute_heading_test_launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='cortex',
+            namespace='test',
+            executable='compute_heading_test',
+            name='bt_root', 
+            parameters=[{'current_object':'cup'}]
+        ),
+        Node(
+            package='chimera_camera',
+            namespace='test',
+            executable='camera_node',
+            name='CameraPublisher',
+            parameters=[{'camera_port':2, 'display_output':True}]
+        ),
+        Node(
+            package='chimera_camera',
+            namespace='test',
+            executable='camera_node_raw',
+            name='CameraPublisherRaw',
+            parameters=[{'camera_port':2, 'fps':30}]
+        )
+    ])
